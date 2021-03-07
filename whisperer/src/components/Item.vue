6e5733fa03470f500c01a8b6012db84b0fba507f
Atsuko Yamada <bigdipper6t@gmail.com>
2021-03-07 22:51:02 +0900

    コンポーネント化

---
+++ whisperer/src/components/Item.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="home">
+        <div class="card p-2">
+            <div class="row">
+                <svg class="col-3 bd-placeholder-img" width="60px" height="60px" xmlns="http://www.w3.org/2000/svg"
+                    preserveAspectRatio="xMidYMid slice" focusable="false" role="img">
+                    <rect width="60px" height="60px" fill="#eee" />
+                </svg>
+                <div class="col-7  align-items-center text-left">
+                    <div class=""> {{whisper.name}}<span class="mr-auto text-muted"> @{{whisper.uid}}</span></div>
+                    <h5 class="card-text"> {{whisper.content}}</h5>
+                </div>
+                <div class="col-2 align-self-center m-0">
+                    <div class="btn btn-outline-primary">!</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import {
+        db
+    } from '../main'
+
+    export default {
+        props: ['id', 'uid'],
+        data() {
+            return {
+                whisper: {},
+                user: {}
+            }
+        },
+        firestore() {
+            return {
+                whisper: db.collection('whispers').doc(this.$props.id),
+                user: db.collection('users').doc(this.$props.uid)
+            }
+        }
+    }
+</script>
--- whisperer/src/views/Home.vue
+++ whisperer/src/views/Home.vue
@@ -1,24 +1,11 @@
 <template>
   <div class="home">
-    <div class="row">
-      <div class="col-md-3 card shadow-sm mx-4 my-1 p-3" v-for="whisper in orderBy(whispers,'date',-1)"
-        :key="whisper.id">
-        <div class="row">
-          <svg class="col-3 bd-placeholder-img" width="60px" height="60px" xmlns="http://www.w3.org/2000/svg"
-            preserveAspectRatio="xMidYMid slice" focusable="false" role="img">
-            <rect width="60px" height="60px" fill="#777" />
-          </svg>
-          <div class="col-9  align-items-center text-left">
-            <div class=""> {{whisper.name}}<span class="mr-auto text-muted"> @{{whisper.uid}}</span></div>
-            <h5 class="card-text"> {{whisper.content}}</h5>
-          </div>
-        </div>
-      </div>
-    </div>
+    <item v-for="whisper in orderBy(whispers,'date',-1)" :key="whisper.id" :id="whisper.id" :uid="whisper.uid" />
   </div>
 </template>
 
 <script>
+  import Item from '@/components/Item.vue'
   import {
     db
   } from '../main'
@@ -26,7 +13,9 @@
 
   export default {
     name: 'home',
-    components: {},
+    components: {
+      Item
+    },
     data() {
       return {
         whispers: []
